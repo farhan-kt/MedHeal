5f777c0ac6970128b9972043d1d5e93792bef359
farhankt46 <farhankt46@gmail.com>
2024-06-12 17:39:19 +0530

    updated

--- lib/view/user/home/user_home.dart
+++ lib/view/user/home/user_home.dart
@@ -33,111 +33,123 @@ class UserHomeScreen extends StatelessWidget {
               horizontal: size.width * .042, vertical: size.height * 0.01),
           child: SingleChildScrollView(
             scrollDirection: Axis.vertical,
-            child:
-                Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
-              homeAppBar(size, context),
-              SizedBox(height: size.height * .02),
-              Consumer<AppointmentProvider>(
-                builder: (context, appointmentProvider, child) {
-                  final upcomingAppointments = appointmentProvider
-                      .allAppointmentList
-                      .where(
-                          (appointment) => isUpcomingAppointment(appointment))
-                      .toList();
-                  if (upcomingAppointments.isEmpty) {
-                    return SizedBox(
-                      height: size.height * .19,
-                      width: size.width * .88,
-                      child: Center(
-                        child: poppinsHeadText(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                homeAppBar(size, context),
+                SizedBox(height: size.height * .02),
+                Consumer<AppointmentProvider>(
+                  builder: (context, appointmentProvider, child) {
+                    final allAppointments =
+                        appointmentProvider.allAppointmentList;
+                    final upcomingAppointments = allAppointments
+                        .where(
+                            (appointment) => isUpcomingAppointment(appointment))
+                        .toList();
+                    upcomingAppointments.sort((a, b) {
+                      final aDateTime = parseDateTime(a.date!, a.time!);
+                      final bDateTime = parseDateTime(b.date!, b.time!);
+                      return aDateTime.compareTo(bDateTime);
+                    });
+                    if (upcomingAppointments.isEmpty) {
+                      return SizedBox(
+                        height: size.height * .19,
+                        width: size.width * .88,
+                        child: Center(
+                          child: poppinsHeadText(
                             text: 'No Upcoming Appointments',
                             color: Color(0xFF1995AD),
-                            fontSize: 18),
-                      ),
-                    );
-                  } else {
-                    final upcomingAppointment = upcomingAppointments.first;
-                    return FutureBuilder<DoctorModel?>(
-                      future:
-                          Provider.of<DoctorProvider>(context, listen: false)
-                              .getDoctorById(upcomingAppointment.docId!),
-                      builder: (context, doctorSnapshot) {
-                        if (doctorSnapshot.connectionState ==
-                            ConnectionState.waiting) {
-                          return const CircularProgressIndicator();
-                        } else if (doctorSnapshot.hasError) {
-                          return Text('Error: ${doctorSnapshot.error}');
-                        } else if (doctorSnapshot.hasData &&
-                            doctorSnapshot.data != null) {
-                          final doctor = doctorSnapshot.data!;
-                          return homeUpcomingAppointment(
-                              size, context, upcomingAppointment, doctor);
-                        } else {
-                          return const Text('Doctor not found');
-                        }
-                      },
-                    );
-                  }
-                },
-              ),
-              SizedBox(height: size.height * .02),
-              poppinsHeadText(
-                text: 'Doctor Speciality',
-              ),
-              SizedBox(height: size.height * .01),
-              homeCategory(context, size,
-                  circleAvatarRadius: circleAvatarRadius),
-              SizedBox(height: size.height * .01),
-              Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
-                poppinsHeadText(text: 'Top Doctors'),
-                TextButton(
-                  onPressed: () {
-                    bottomProvider.userOnTap(2);
+                            fontSize: 18,
+                          ),
+                        ),
+                      );
+                    } else {
+                      final nextUpcomingAppointment =
+                          upcomingAppointments.first;
+                      return FutureBuilder<DoctorModel?>(
+                        future:
+                            Provider.of<DoctorProvider>(context, listen: false)
+                                .getDoctorById(nextUpcomingAppointment.docId!),
+                        builder: (context, doctorSnapshot) {
+                          if (doctorSnapshot.connectionState ==
+                              ConnectionState.waiting) {
+                            return const CircularProgressIndicator();
+                          } else if (doctorSnapshot.hasError) {
+                            return Text('Error: ${doctorSnapshot.error}');
+                          } else if (doctorSnapshot.hasData &&
+                              doctorSnapshot.data != null) {
+                            final doctor = doctorSnapshot.data!;
+                            return homeUpcomingAppointment(
+                                size, context, nextUpcomingAppointment, doctor);
+                          } else {
+                            return const Text('Doctor not found');
+                          }
+                        },
+                      );
+                    }
                   },
-                  child: poppinsHeadText(
-                    text: 'SEE ALL',
-                    fontSize: 14,
-                    color: const Color(0xFF1995AD),
-                  ),
-                )
-              ]),
-              SizedBox(height: size.height * .01),
-              Consumer<DoctorProvider>(
-                builder: (context, doctorValue, child) {
-                  if (doctorValue.isLoading) {
-                    return const Center(child: CircularProgressIndicator());
-                  } else {
-                    if (doctorValue.allDoctorList.isNotEmpty) {
-                      final allDoctor = doctorValue.allDoctorList;
+                ),
+                SizedBox(height: size.height * .02),
+                poppinsHeadText(text: 'Doctor Speciality'),
+                SizedBox(height: size.height * .01),
+                homeCategory(context, size,
+                    circleAvatarRadius: circleAvatarRadius),
+                SizedBox(height: size.height * .01),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    poppinsHeadText(text: 'Top Doctors'),
+                    TextButton(
+                      onPressed: () {
+                        bottomProvider.userOnTap(2);
+                      },
+                      child: poppinsHeadText(
+                        text: 'SEE ALL',
+                        fontSize: 14,
+                        color: const Color(0xFF1995AD),
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(height: size.height * .01),
+                Consumer<DoctorProvider>(
+                  builder: (context, doctorValue, child) {
+                    if (doctorValue.isLoading) {
+                      return const Center(child: CircularProgressIndicator());
+                    } else {
+                      if (doctorValue.allDoctorList.isNotEmpty) {
+                        final allDoctor = doctorValue.allDoctorList;
 
-                      return ListView.builder(
-                        shrinkWrap: true,
-                        physics: const NeverScrollableScrollPhysics(),
-                        itemCount: allDoctor.length,
-                        itemBuilder: (context, index) {
-                          final doctors = allDoctor[index];
-                          return Column(
-                            children: [
-                              AllDoctorsContainer(
+                        return ListView.builder(
+                          shrinkWrap: true,
+                          physics: const NeverScrollableScrollPhysics(),
+                          itemCount: allDoctor.length,
+                          itemBuilder: (context, index) {
+                            final doctors = allDoctor[index];
+                            return Column(
+                              children: [
+                                AllDoctorsContainer(
                                   size: size,
                                   isAdmin: false,
                                   doctors: doctors,
                                   value: doctorValue,
-                                  circleAvatarRadius: circleAvatarRadius),
-                              SizedBox(height: size.height * .02),
-                            ],
-                          );
-                        },
-                      );
-                    } else {
-                      return Center(
-                          child:
-                              Image.asset('assets/no doctors available.png'));
+                                  circleAvatarRadius: circleAvatarRadius,
+                                ),
+                                SizedBox(height: size.height * .02),
+                              ],
+                            );
+                          },
+                        );
+                      } else {
+                        return Center(
+                          child: Image.asset('assets/no doctors available.png'),
+                        );
+                      }
                     }
-                  }
-                },
-              ),
-            ]),
+                  },
+                ),
+              ],
+            ),
           ),
         ),
       ),
@@ -155,5 +167,12 @@ class UserHomeScreen extends StatelessWidget {
       log('Error parsing date and time: $e');
       return false;
     }
+  }
+
+  DateTime parseDateTime(String date, String time) {
+    final dateFormat = DateFormat('dd/MM/yyyy hh:mm a');
+    String dateTimeStr = '$date $time';
+    dateTimeStr = dateTimeStr.replaceAll('\u202F', ' ');
+    return dateFormat.parse(dateTimeStr);
   }
 }
